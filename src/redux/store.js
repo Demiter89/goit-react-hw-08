9d377f3d656e1309a4e0118646b5a22539a23b61
Maksym Shevchuk <maksymshevchuk89@gmai.com>
2025-04-07 19:43:45 +0200

    add index file

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import contactsReducer from './contactsSlice';
-import filtersReducer from './filtersSlice';
+import contactsReducer from '../redux/contacts/slice';
+import filtersReducer from '../redux/filters/filtersSlice';
 import authReducer from './auth/authSlice';
 
 const authPersistConfig = {
